203cd8102ffed29b18facc6a488f811f10fc525e
yawlad33 <yawlad33@gmail.com>
2021-06-27 12:32:50 +0300

    laba_7_1
    laba_7_1

---
+++ My_7_1/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+double garmon(double x, double y) {
+    return 2.0 / (1.0 / x + 1.0 / y);
+}
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+
+
+    double x, y;
+    cout << "¬ведите два числа: ";
+    cin >> x >> y;
+    cout << garmon(x, y);
+
+
+	return 0;
+}
